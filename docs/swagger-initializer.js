61bd7c0f011a3230c16b0bd3059cf0b4eb985e56
Bastien TURCO <bastienturco@wanadoo.fr>
2024-10-23 15:12:23 +0200

    test embedded openAPI

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "petstore.json",
+    url: "./embed/embedagain/embedfinal/petstore.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
